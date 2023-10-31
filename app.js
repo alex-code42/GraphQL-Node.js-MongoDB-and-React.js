d42f51ffac820454034e84d2af9a1b57fd289f93
Alex <areichel1986@gmail.com>
2023-10-31 19:29:59 +0100

    ydah

--- app.js
+++ app.js
@@ -1,8 +1,68 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require("express")
+var { graphqlHTTP } = require("express-graphql")
+var { buildSchema } = require("graphql")
 
-const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000.');
-});
+
+const events = [];
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+// The root provides a resolver function for each API endpoint
+var event = {
+  events: () => {
+    return events
+  },
+    createEvent: (args) => {
+        const event = { 
+            _id: Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
+    }}
+
+
+var app = express()
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+      type RootQuery {
+        events: [Event!]!
+      }
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: event,
+    graphiql: true,
+  })
+)
+app.listen(4000)
+console.log("Running a GraphQL API server at http://localhost:4000/graphql")
